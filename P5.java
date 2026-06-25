67a7d9dd847c93e910615ccb13d9d7ace7b2b7f1
Gobbarakallu surendra <sgobbarakallu@gmail.com>
2026-06-25 21:32:46 +0530

    Add files via upload

---
+++ P5.java
@@ -0,0 +1,18 @@
+class P5{
+
+
+public static void main(String args[]){
+    int n=4;
+for(int i=1;i<=n;i++){
+    
+        for(int k=0;k<n-i;k++){
+            System.out.print(" ");
+        }
+        for(int k=0;k<i;k++){
+
+        System.out.print("*");
+    }
+    System.out.println();
+}
+}
+}
